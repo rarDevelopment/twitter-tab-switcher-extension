efbdabba22b81f66a1b7c0a5e1b9ecb67638f518
Robert R <rarDevelopment@users.noreply.github.com>
2023-01-20 02:11:27 -0500

    removed focus event handler, check to make sure you're on the home page

--- twit.js
+++ twit.js
@@ -1,10 +1,10 @@
 const timeToWait = 2000;
 
-window.addEventListener('focus', function (event) {
-    setTimeout(() => {
-        clickFollowingTab();
-    }, timeToWait);
-});
+// window.addEventListener('focus', function (event) {
+//     setTimeout(() => {
+//         clickFollowingTab();
+//     }, timeToWait);
+// });
 
 window.addEventListener('load', function (event) {
     setTimeout(() => {
@@ -14,5 +14,7 @@ window.addEventListener('load', function (event) {
 
 
 function clickFollowingTab() {
-    Array.prototype.slice.call(document.getElementsByTagName('span')).filter(s => s.innerHTML === 'Following')[0].click();
+    if (window.location.href.endsWith('/home')) {
+        Array.prototype.slice.call(document.getElementsByTagName('span')).filter(s => s.innerHTML === 'Following')[0].click();
+    }
 }
