578c7e7b910e9262eb9ca7b4d1d2cd8bb224c4fb
Robert R <rarDevelopment@users.noreply.github.com>
2023-01-21 23:36:38 -0500

    removing more junk

--- background.js
+++
@@ -1,8 +0,0 @@
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     var activeTab = tabs[0];
-//     if (activeTab.url.includes("twitter.com")) {
-//         var scriptNode = document.createElement('script');
-//         scriptNode.textContent = `Array.prototype.slice.call(document.getElementsByTagName('span')).filter(s => s.innerHTML === 'Following')[0].click();`;
-//         document.body.appendChild(scriptNode);
-//     }
-// });
