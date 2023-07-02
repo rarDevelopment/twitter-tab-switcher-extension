5f0a1eba5658f0af25bc182272f318fb08c5c8f9
Robert R <rarDevelopment@users.noreply.github.com>
2023-07-02 09:34:04 -0400

    work in progress, untested?

--- twit.js
+++ twit.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function (event) {
 
 
 function clickFollowingTab() {
-    if (window.location.href.endsWith('/home')) {
+    //if (window.location.href.endsWith('/home')) {
         Array.prototype.slice.call(document.getElementsByTagName('span')).filter(s => s.innerHTML === 'Following')[0].click();
-    }
+    //}
 }
